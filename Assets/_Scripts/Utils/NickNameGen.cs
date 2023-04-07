8ab647dbf4a389569c49200a9230d70e055f9fbd
Pavel <oavelasdqwe8@gmail.com>
2023-04-07 14:30:47 +0300

    fix nickName

--- Assets/_Scripts/Utils/NickNameGen.cs
+++ Assets/_Scripts/Utils/NickNameGen.cs
@@ -12,7 +12,7 @@ namespace _Scripts.Utils
 			if (req.result == UnityWebRequest.Result.Success)
 			{
 				var rawRes = req.downloadHandler.text;
-				return rawRes.Substring(2, rawRes.Length - 4);
+				return rawRes.Substring(2, rawRes.Length - 4).Replace('_', ' ');
 			}
 			return $"Player {Random.Range(0, 100)}";
 		}
